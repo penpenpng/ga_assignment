6df9b3583235a564c55795a7785dc17988a6b034
poman <poman1638m@gmail.com>
2019-06-13 21:27:19 +0900

    improve run.bat for simplicity

--- run.bat
+++ run.bat
@@ -1,11 +1,17 @@
 @echo off
+setlocal
+  if exist a.exe (
+    del a.exe
+  )
 
-if exist a.exe (
-  del a.exe
-)
+  gcc submit.c -D _WIN64
+  if not exist a.exe goto :EOF
 
-gcc submit.c -D _WIN64
+  if "%1" == "" (
+    set data=data\input_sqrt_100
+  ) else (
+    set data=%1
+  )
 
-if exist a.exe (
-  a.exe %1
-)
+  a.exe %data% > log
+endlocal
